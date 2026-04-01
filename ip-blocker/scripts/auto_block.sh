6671ce4379f192729c5ec5b37b01297c0dd7a935
Nikita <nikitamulam2005@gmail.com>
2026-04-01 20:49:25 +0530

    root user changed

--- ip-blocker/scripts/auto_block.sh
+++ ip-blocker/scripts/auto_block.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Step 1: Run AI detection
-python3 /home/ubuntu/idps/ip-blocker/scripts/ai_detect.py
+python3 /home/nikitamulam2005/idps/ip-blocker/scripts/ai_detect.py
 
 # Step 2: Apply dynamic blocking
-bash /home/ubuntu/idps/ip-blocker/scripts/dynamic_block.sh
+bash /home/nikitamulam2005/idps/ip-blocker/scripts/dynamic_block.sh
